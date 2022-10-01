0f0005e48a5c57fd83c4178b8100d0ca5637fe9a
Pranshu1902 <pranshu1902@gmail.com>
2022-10-01 13:35:05 +0530

    integrated routes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require("./app");
 
 const port = process.env.PORT || 5000;
+
+const routes = require("./router/user");
+
+app.use("/api", routes);
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
