3e6c90c5ced535d027b185eaa2557d199ce4c2cc
Rxghavdev <mattaraghav67@gmail.com>
2022-10-01 13:28:16 +0530

    added routes

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const app = require("./app");
-const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB Connected"));
-//middlewares
-app.use(bodyParser.json());
-app.use(cors());
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
---
+++ router/user.js
@@ -0,0 +1,34 @@
+const express=require("express")
+const router=express.Router()
+
+router.get("/", (req, res) => {
+    res.send("userlist");                      
+  });
+  router.get("/new", (req, res) => {
+    res.send("usernew");                
+  });
+  router.post("/",(req,res)=>{
+    res.send("create user")
+  })
+  router.route("/:id").get((req,res)=>{        //advanced routing used to get id of the user can be userid too not predefinded
+    //console.log(req.user)
+    res.send(`Get user id ${req.params.id}`)
+  })
+  .put((req,res)=>{        //advanced routing used to get id of the user can be userid too not predefinded
+    req.params.id
+    res.send(`update user id ${req.params.id}`)
+  })
+  .delete((req,res)=>{        //advanced routing used to get id of the user can be userid too not predefinded
+    req.params.id
+    res.send(`delete user id ${req.params.id}`)
+  })
+
+  
+  const users=[{name: "rohan"},{name:"sally"}]
+  router.param("id",(req,res,next,id)=>{
+    req.user=users[id]
+    next()                            //next fn 
+  })
+
+  
+  module.exports=router
