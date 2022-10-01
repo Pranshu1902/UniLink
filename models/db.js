608657db5f3dcec27e602a8b399922aa23153283
Pranshu1902 <pranshu1902@gmail.com>
2022-10-02 01:14:42 +0530

    completed CRUD for college

--- models/db.js
+++ models/db.js
@@ -5,11 +5,11 @@ const connectDB = async () => {
     const conn = await mongoose.connect(
       "mongodb+srv://raghavmatta:Helloitsme@cluster0.zpccwjh.mongodb.net/?retryWrites=true&w=majority"
     );
-    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline); //VARIABLE CONN THEN CONNECTING WITH ENV FILE
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
     process.exit(1);
-  } //creating fn using arrow method normal method only no rs
+  }
 };
 
 module.exports = connectDB;
