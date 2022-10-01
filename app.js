33e514a12c4c543a0f2c684c63634c4444942e5b
Pranshu1902 <pranshu1902@gmail.com>
2022-10-01 14:03:01 +0530

    refactor and added basic fe

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 const express = require("express");
-
 const app = express();
-
-app.get("/", function (request, response) {
-  response.send("Hello world");
-});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 5000;
 
 const routes = require("./router/user");
 
+// home page route added
+app.get("/", function (request, response) {
+  response.sendFile("home.html", { root: __dirname });
+});
+
+// adding api routes
 app.use("/api", routes);
 
 app.listen(port, () => {
