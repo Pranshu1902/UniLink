ea67bc56817411a5c69272ab769f585b4bd4d0c8
Pranshu1902 <pranshu1902@gmail.com>
2022-11-10 01:38:14 +0530

    removed duplicate fields

--- models/models.js
+++ models/models.js
@@ -189,10 +189,6 @@ const studentSchema = new Schema(
       type: String,
       required: false,
     },
-    email: {
-      type: String,
-      required: false,
-    },
     twitter: {
       type: String,
       required: false,
